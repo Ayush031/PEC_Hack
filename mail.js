de0ae5352f27c7926cc3b960e65257f395548b63
Ayush <ayushkansal303@gmail.com>
2023-04-22 20:59:17 +0530

    form testing

--- mail.js
+++ mail.js
@@ -1,4 +1,14 @@
-import firebaseConfig from "./firebaseConfig"
+// import firebaseConfig from "./firebaseConfig"
+const firebaseConfig = {
+    apiKey: "AIzaSyADwA6l2cK_BBn_dJgwJGmZ1YD8cCBEfX4",
+    authDomain: "pechack-4826f.firebaseapp.com",
+    databaseURL: "https://pechack-4826f-default-rtdb.firebaseio.com",
+    projectId: "pechack-4826f",
+    storageBucket: "pechack-4826f.appspot.com",
+    messagingSenderId: "986323968038",
+    appId: "1:986323968038:web:ed2dc74ef16a3bab3d46e7",
+    measurementId: "G-E3HRQXQMRZ"
+};
 
 //initialize database
 firebase.initializeApp(firebaseConfig);
@@ -35,7 +45,6 @@ const saveMsg = (fname, lname, subject) => {
         lname: lname,
         subject: subject,
     });
-
 };
 
 const getElementVal = (id) => {
