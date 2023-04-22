1866dce3288e96b9ffa594335289eb8636b824ff
Ayush <ayushkansal303@gmail.com>
2023-04-22 13:47:14 +0530

    created alert in javascript code

--- mail.js
+++ mail.js
@@ -16,26 +16,29 @@ var pechackDB = firebase.database().ref('pechack')
 
 document.getElementById("pechack").addEventListener("submit", submitForm);
 
-function submitForm(e){
+function submitForm(e) {
     e.preventDefault();
-    var firstname= getElementVal("fname");
-    var lastname= getElementVal("lname");
-    var subject= getElementVal("subject");
+    var firstname = getElementVal("fname");
+    var lastname = getElementVal("lname");
+    var subject = getElementVal("subject");
 
     saveMsg(firstname, lastname, subject);
+
+    //enable alert after submission
+    document.querySelector('.alert').style.display = "block";
 }
 
 const saveMsg = (fname, lname, subject) => {
- var newPechack= pechackDB.push();
+    var newPechack = pechackDB.push();
 
- newPechack.set({
-    fname: fname,
-    lname: lname,
-    subject: subject,
- });
+    newPechack.set({
+        fname: fname,
+        lname: lname,
+        subject: subject,
+    });
 
 };
 
-const getElementVal=(id)=>{
+const getElementVal = (id) => {
     return document.getElementById(id).value;
 }
