704b68df0b6b8d4f74827522c15bcd909be7ec27
Ayush <ayushkansal303@gmail.com>
2023-04-22 21:08:32 +0530

    mail.js edited

--- mail.js
+++ mail.js
@@ -1,14 +1,14 @@
-// import firebaseConfig from "./firebaseConfig"
-const firebaseConfig = {
-    apiKey: "AIzaSyADwA6l2cK_BBn_dJgwJGmZ1YD8cCBEfX4",
-    authDomain: "pechack-4826f.firebaseapp.com",
-    databaseURL: "https://pechack-4826f-default-rtdb.firebaseio.com",
-    projectId: "pechack-4826f",
-    storageBucket: "pechack-4826f.appspot.com",
-    messagingSenderId: "986323968038",
-    appId: "1:986323968038:web:ed2dc74ef16a3bab3d46e7",
-    measurementId: "G-E3HRQXQMRZ"
-};
+import firebaseConfig from "./firebaseConfig"
+// const firebaseConfig = {
+//     apiKey: "AIzaSyADwA6l2cK_BBn_dJgwJGmZ1YD8cCBEfX4",
+//     authDomain: "pechack-4826f.firebaseapp.com",
+//     databaseURL: "https://pechack-4826f-default-rtdb.firebaseio.com",
+//     projectId: "pechack-4826f",
+//     storageBucket: "pechack-4826f.appspot.com",
+//     messagingSenderId: "986323968038",
+//     appId: "1:986323968038:web:ed2dc74ef16a3bab3d46e7",
+//     measurementId: "G-E3HRQXQMRZ"
+// };
 
 //initialize database
 firebase.initializeApp(firebaseConfig);
